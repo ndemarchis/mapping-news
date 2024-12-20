c7609cbb318e019301025c11c2a4fed30fd8f8a0
Nick DeMarchis <ned004@bucknell.edu>
2024-12-20 15:17:22 -0500

    L O L

--- app/linknyc/openlayers/page.tsx
+++ app/linknyc/openlayers/page.tsx
@@ -4,12 +4,11 @@ import React, { useState, useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
-import { toStringXY } from "ol/coordinate";
 import VectorSource from "ol/source/Vector";
 import GeoJSON from "ol/format/GeoJSON";
 import VectorLayer from "ol/layer/Vector";
 
-export function MapComponent() {
+export default function Openlayers() {
   const [map, setMap] = useState();
   const mapElement = useRef<HTMLDivElement>();
   const mapRef = useRef();
@@ -41,20 +40,13 @@ export function MapComponent() {
     });
     return () => map.setTarget(undefined);
   }, []);
-
-  return (
-    <div
-      style={{ height: "100%", width: "100%" }}
-      ref={mapElement as React.RefObject<HTMLDivElement>}
-      className="map-container"
-    />
-  );
-}
-
-export default function Openlayers() {
   return (
     <div className="flex h-full min-h-[calc(100vh-6rem)] w-full flex-col items-center justify-center pt-16">
-      <MapComponent />
+      <div
+        style={{ height: "100%", width: "100%" }}
+        ref={mapElement as React.RefObject<HTMLDivElement>}
+        className="map-container"
+      />
     </div>
   );
 }
