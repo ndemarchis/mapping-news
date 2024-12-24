d5b4438fa1ee5a71e5ad8ef7fe5c7e6c215e5a63
Nick DeMarchis <ned004@bucknell.edu>
2024-12-24 13:44:39 -0500

    contact info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,45 +16,31 @@ export default function About() {
           includes:
           <ul className="list-disc pl-4">
             <li>
-              <Link href="https://tailwindcss.com/" rel="nofollow">
-                Tailwind CSS
-              </Link>{" "}
+              <Link href="https://tailwindcss.com/">Tailwind CSS</Link>{" "}
               –&nbsp;Utility-first CSS framework for rapid UI development
             </li>
             <li>
-              <Link href="https://www.radix-ui.com/" rel="nofollow">
-                Radix
-              </Link>{" "}
+              <Link href="https://www.radix-ui.com/">Radix</Link>{" "}
               –&nbsp;Primitives like modal, popover, etc. to build a stellar
               user experience
             </li>
             <li>
-              <Link href="https://framer.com/motion" rel="nofollow">
-                Framer Motion
-              </Link>{" "}
+              <Link href="https://framer.com/motion">Framer Motion</Link>{" "}
               –&nbsp;Motion library for React to animate components with ease
             </li>
             <li>
-              <Link href="https://lucide.dev/" rel="nofollow">
-                Lucide
-              </Link>{" "}
-              –&nbsp;Beautifully simple, pixel-perfect icons
+              <Link href="https://lucide.dev/">Lucide</Link> –&nbsp;Beautifully
+              simple, pixel-perfect icons
             </li>
             <li>
-              <Link
-                href="https://nextjs.org/docs/basic-features/font-optimization"
-                rel="nofollow"
-              >
+              <Link href="https://nextjs.org/docs/basic-features/font-optimization">
                 <code>next/font</code>
               </Link>{" "}
               –&nbsp;Optimize custom fonts and remove external network requests
               for improved performance
             </li>
             <li>
-              <Link
-                href="https://nextjs.org/docs/app/api-reference/functions/image-response"
-                rel="nofollow"
-              >
+              <Link href="https://nextjs.org/docs/app/api-reference/functions/image-response">
                 <code>ImageResponse</code>
               </Link>{" "}
               – Generate dynamic Open Graph images at the edge
@@ -62,6 +48,14 @@ export default function About() {
           </ul>
         </li>
       </ul>
+      <h1 className="mt-8 text-3xl font-bold">contact</h1>
+      <p>
+        email{" "}
+        <span className="font-semibold text-gray-600 underline-offset-2">
+          hi [at] mapping.news
+        </span>
+        .
+      </p>
     </div>
   );
 }
