aadd4d57d65337e20afb1a8da66d9ba9a60fa134
Nick DeMarchis <ned004@bucknell.edu>
2024-12-27 14:57:51 -0500

    spaghettification

--- actions/feedParser.py
+++ actions/feedParser.py
@@ -1,4 +1,4 @@
-from typing import List, TypedDict, Optional, Set
+from typing import List, TypedDict, Optional, Set, Dict
 from pathlib import Path
 import json
 import asyncio
@@ -10,38 +10,58 @@ from pydantic import BaseModel
 from openai import OpenAI
 import pandas as pd
 import requests
+from supabase import create_client, Client
+import uuid
 
 FEED_FILE = Path(__file__).resolve().parent / "../public/feeds/feeds.csv"
 CACHE_FILE = Path(__file__).resolve().parent / "../cache.json"
-FULL_ARTICLE_CHAR_THRESHOLD = 250
 
 class GeocodedLocation(TypedDict):
     lat: float
     lon: float
 
+class GeoBoundaries(TypedDict):
+    minLat: float
+    minLon: float
+    maxLat: float
+    maxLon: float
+
+class Feed(TypedDict):
+    name: str
+    url: str
+    geo: Optional[GeoBoundaries]
+
 class FeedItem(TypedDict, total=False):
     title: Optional[str]
     link: Optional[str]
-    pubDate: Optional[str]
+    pub_date: Optional[str]
     id: Optional[str]
     author: Optional[str]
     fullText: Optional[bool]
     content: Optional[str]
+    feed: Optional[Feed]
+
+GeocodedLocations = Dict[str, Optional[GeocodedLocation]]
+"""Maps location name to the geocoded location."""
+
+GeocodingCache = Dict[str, Dict[str, GeocodedLocation]]
+"""Caches location name, then boundaries to the actual location that it should resolve to."""
 
 class CustomFeedItem(TypedDict):
     item: FeedItem
-    locations: Optional[List[str]]
-    geocoded_locations: Optional[List[GeocodedLocation]]
+    locations: GeocodedLocations
 
 class AddressArray(BaseModel):
     locations: List[str]
 
-class FeedParser:
-    def __init__(self):
-        self.articles = []
-        self.custom_articles = []
-        self.api_key = os.getenv("OPENAI_API_KEY")
-        self.client = OpenAI(api_key=self.api_key)
+class SupabaseRow(TypedDict):
+    locations: Optional[GeocodedLocations]
+    title: Optional[str]
+    link: Optional[str]
+    id: Optional[str]
+    pub_date: Optional[str]
+    author: Optional[str]
+    feed_name: Optional[str]
 
 def read_file_csv(file_path: Path) -> pd.DataFrame:
     df = pd.read_csv(file_path)
@@ -58,12 +78,11 @@ def write_file(file_path: Path, data: str) -> None:
 def file_exists(file_path: Path) -> bool:
     return file_path.exists()
 
-async def load_feeds() -> List[str]:
-    """
-    Get the list of RSS feeds from the file.
-    """
+async def load_feeds() -> List[Feed]:
+    """Get the list of RSS feeds from the file."""
     data = read_file_csv(FEED_FILE)
-    return data["url"].tolist()
+    return data.to_dict(orient="records")
+    
 
 async def load_seen_articles() -> Set[str]:
     # TODO: get data in from the server
@@ -76,67 +95,61 @@ async def save_seen_articles(seen_articles: Set[str]) -> None:
     write_file(CACHE_FILE, json.dumps(list(seen_articles), ensure_ascii=False))
 
 def feed_item_standardizer(item: dict) -> FeedItem:
-    """
-    Standardize the individual item results into a predictable, library-agnostic format.
-    """
+    """Standardize the individual item results into a predictable, library-agnostic format."""
     return {
         "title": item.get("title"),
         "link": item.get("link"),
-        "pubDate": item.get("published"),
+        "pub_date": item.get("published"),
         "id": item.get("id"),
         "author": item.get("author"),
         "fullText": False,
-        # "fullText": len(item.get("summary", "")) > FULL_ARTICLE_CHAR_THRESHOLD,
     }
 
-async def parse_feed(url: str) -> List[FeedItem]:
-    """
-    For a given RSS feed URL, get all current items.
-    """
+async def parse_feed(feed: Feed) -> List[FeedItem]:
+    """For a given RSS feed URL, get all current items."""
+
+    url = feed.get("url", "")
+
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             content = await response.text()
-    feed = feedparser.parse(content)
-    return [feed_item_standardizer(item) for item in feed.entries]
+    parsedFeed = feedparser.parse(content)
+    return [{**feed_item_standardizer(item), "feed": feed} for item in parsedFeed.entries]
 
 async def fetch_new_articles() -> List[FeedItem]:
-    """
-    Check RSS feeds for new articles that are not included in the local cache.
-    """
+    """Check RSS feeds for new articles that are not included in the local cache."""
     seen_articles = await load_seen_articles()
     new_articles: List[FeedItem] = []
 
-    RSS_FEEDS = await load_feeds()
+    feeds = await load_feeds()
 
-    for feed_url in RSS_FEEDS:
-        articles = await parse_feed(feed_url)
+    for feed in feeds:
+        articles = await parse_feed(feed)
         for article in articles:
             if not article.get("id"):
                 continue
             if article["id"] not in seen_articles:
                 new_articles.append(article)
-                seen_articles.add(article["id"])
+                seen_articles.add(article.get("id") or "")
 
     await save_seen_articles(seen_articles)
     return new_articles
 
 async def scrap(url: str) -> str:
-    """
-    Get data from provided article URLs.
-    """
+    """Get data from provided article URLs."""
     article = Article(url)
     article.download()
     article.parse()
     return article.text
 
 async def add_articles_full_content(articles: List[FeedItem]) -> List[FeedItem]:
-    """
-    For those articles that are missing full text, fetch the full content.
-    """
+    """For those articles that are missing full text, fetch the full content."""
     async def process_article(article: FeedItem) -> FeedItem:
-        if article.get("fullText"):
+        if article.get("fullText") or not article.get("link"):
             return article
-        content = await scrap(article.get("link", ""))
+        content = await scrap(article.get("link") or "")
+
+        print(f"- Scraping {article.get('title', '')}")
 
         # TODO: do we want to add other information here?
         article["content"] = content
@@ -145,43 +158,38 @@ async def add_articles_full_content(articles: List[FeedItem]) -> List[FeedItem]:
     return await asyncio.gather(*(process_article(article) for article in articles))
 
 def add_article_locations(articles: List[FeedItem]) -> List[CustomFeedItem]:
-    """
-    For each article, extract location information.
-    """
+    """For each article, extract location information."""
     return [add_article_location(article) for article in articles]
 
 def filter_parsed_locations(locations: List[str]) -> List[str]:
-    """
-    Remove locations with fewer than 3 words.
-    """
+    """Remove locations with fewer than 3 words."""
     return [location for location in locations if len(location.split()) > 2]
 
 def add_article_location(article: FeedItem) -> CustomFeedItem:
-    # TODO: Either add API or spaCy to get locations
-
     key = os.getenv("OPENAI_API_KEY")
     client = OpenAI(api_key=key)
+
+    print(f"- OpenAI Extraction for {article.get('title', '')}")
 
     completion = client.beta.chat.completions.parse(
         model="gpt-4o-mini-2024-07-18",
         messages=[
             {"role": "system", "content": "Extract all physical addresses from the text."},
-            {"role": "user", "content": article.get("content", "")},
+            {"role": "user", "content": article.get("content") or ""},
         ],
         response_format=AddressArray,
     )
 
     event = completion.choices[0].message.parsed
+    filtered_locations = filter_parsed_locations(event.locations if event else [])
 
     return {
         "item": article,
-        "locations": filter_parsed_locations(event.locations),
+        "locations": dict.fromkeys(filtered_locations, None),
     }
 
 def add_geocoded_locations(articles: List[CustomFeedItem]) -> List[CustomFeedItem]:
-    """
-    For each article, geocode the locations.
-    """
+    """For each article, geocode the locations."""
     return [add_geocoded_location(article) for article in articles]
 
 def add_geocoded_location(article: CustomFeedItem) -> CustomFeedItem:
@@ -189,20 +197,22 @@ def add_geocoded_location(article: CustomFeedItem) -> CustomFeedItem:
     geocoded_locations = geocode_locations(locations)
     return {
         "item": article["item"],
-        "locations": locations,
-        "geocoded_locations": geocoded_locations,
+        "locations": geocoded_locations,
     }
 
-def geocode_locations(locations: List[str]) -> List[GeocodedLocation]:
-    """
-    For each location, make a request to Google Maps API to get geocoding information.
-    """
-    return [geocode_location(location) for location in locations]
+def geocode_locations(locations: GeocodedLocations) -> GeocodedLocations:
+    """For each location, make a request to Google Maps API to get geocoding information."""
+    
+    returned_locations: GeocodedLocations = {}
+    for location in locations:
+        geocoded_location = geocode_location(location)
+        if geocoded_location:
+            returned_locations[location] = geocoded_location
+
+    return returned_locations
 
 def geocode_location(location: str) -> GeocodedLocation | None:
-    """
-    Make a request to Google Maps API to get geocoding information.
-    """
+    """Make a request to Google Maps API to get geocoding information."""
     key = os.getenv("GOOGLE_MAPS_API_KEY")
     url = "https://maps.googleapis.com/maps/api/geocode/json"
     params = {
@@ -210,16 +220,48 @@ def geocode_location(location: str) -> GeocodedLocation | None:
         "address": location,
     }
 
+    print(f"- Locating {location}")
+
     response = requests.get(url, params=params)
     data = response.json()
     if response.status_code != 200 or data["status"] != "OK":
-        return
+        return None
+    
+    # TODO: Add returned locations to cache
+    # TODO: Filter those locations that aren't specific to an address
+
     lat = data["results"][0]["geometry"]["location"]["lat"]
     lon = data["results"][0]["geometry"]["location"]["lng"]
     return {
         "lat": lat,
         "lon": lon,
     }
+
+def has_valid_location(item: CustomFeedItem) -> bool:
+    """Filter out feed items that don't have any locations."""
+    return bool(item.get("locations"))
+
+def narrow_feed_items(items: List[CustomFeedItem]) -> List[SupabaseRow]:
+    """Filter out feed items that don't have any locations."""
+    return [{
+        "locations": item.get("locations"), 
+        "title": item["item"].get("title"),
+        "link": item["item"].get("link"),
+        "id": str(uuid.uuid3(uuid.NAMESPACE_URL, item["item"].get("id") or "")),
+        "pub_date": item["item"].get("pub_date"),
+        "author": item["item"].get("author"),
+        "feed_name": item["item"]["feed"].get("name") if item["item"]["feed"] else None,
+    } for item in items if has_valid_location(item)]
+
+def send_to_db(articles: List[CustomFeedItem]) -> None:
+    """Send articles to the Supabase database."""
+    supabase_url = os.getenv("SUPABASE_URL") or ""
+    supabase_key = os.getenv("SUPABASE_API_KEY") or ""
+
+    supabase = create_client(supabase_url, supabase_key)
+    narrowed_articles = narrow_feed_items(articles)
+
+    supabase.table("articles_and_data").insert(narrowed_articles).execute()
 
 
 async def main() -> None:
@@ -236,6 +278,9 @@ async def main() -> None:
 
             # save file as JSON
             write_file(Path(__file__).resolve().parent / "../public/feeds/cache.json", json.dumps(new_articles_with_geocoded_locations, ensure_ascii=False))
+
+            # send to db
+            send_to_db(new_articles_with_geocoded_locations)
 
         else:
             print("No new articles found.")
