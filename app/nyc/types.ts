d206ab063035c25fc2b0ab367f819ee099109d0d
Nick DeMarchis <ned004@bucknell.edu>
2024-12-31 15:53:33 -0500

    open graph hack

--- app/nyc/types.ts
+++ app/nyc/types.ts
@@ -4,3 +4,9 @@ export type ArticleDefinition =
   Database["public"]["Tables"]["articles"]["Row"] & {
     location_name: string | null;
   };
+
+export type ArticlesDefinition = {
+  address: string | null;
+  place_id: string | null;
+  articles: ArticleDefinition[];
+} | null;
