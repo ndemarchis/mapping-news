afc193f129b78b43d3dfbb21afce268a518e1268
Nick DeMarchis <ned004@bucknell.edu>
2024-12-31 11:45:40 -0500

    quick meta

--- app/nyc/layout.tsx
+++ app/nyc/layout.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { Metadata } from "next";
 import "ol/ol.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
+
+export const metadata: Metadata = {
+  title: "live news articles in nyc",
+};
