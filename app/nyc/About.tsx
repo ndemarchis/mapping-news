8bd7f4da3946ce34cee8c7effca6711ec4ce468c
Nick DeMarchis <ned004@bucknell.edu>
2025-02-20 18:12:04 -0500

    copy 1

--- app/nyc/About.tsx
+++ app/nyc/About.tsx
@@ -2,101 +2,160 @@ import Link from "@/components/shared/link";
 
 const About = () => (
   <div className="items-left z-10 m-auto flex min-h-screen w-full flex-col justify-start gap-4 p-4 *:z-10 md:max-w-xl">
-    <h2 className="mx-auto">about this map</h2>
+    <h2 id="about-this-map">about this map</h2>
     <p>
-      this map was developed by{" "}
-      <Link href="https://nickdemarchis.com">nick deMarchis</Link> over the
-      course of a few weeks as a proof-of-concept to map places mentioned in
-      local news articles in realtime in New York City.
+      <Link href="https://nickdemarchis.com/">Nick DeMarchis</Link> developed
+      this page as a proof-of-concept to map New York locations mentioned in
+      local news articles in near-realtime.{" "}
     </p>
     <p>
-      it seems like it&apos;s worked, at least to some degree. there&apos;s some
-      dust and clear areas for improvement that i have documented, obviously.
-      check the{" "}
+      While the map functions as intended, there are clear areas of improvement.
+      Check the{" "}
       <Link href="https://github.com/ndemarchis/mapping-news/issues">
         issue tracker
       </Link>{" "}
-      to see what&apos;s up. or, you can contact me with the information
-      mentioned at the bottom of{" "}
-      <Link href="/about" sameWindow>
-        the about page
+      for a list of outstanding bugs. If you’ve found a new bug, or have feature
+      requests, can contact me using the information found at the bottom of{" "}
+      <Link href="http://localhost:3000/about">the about page</Link>.
+    </p>
+    <h2 id="why">why</h2>
+    <h2 id="technical-details">technical details</h2>
+    <h3 id="overview">overview</h3>
+    <ul className="list-disc pl-4">
+      <li>
+        <strong>frontend</strong> —{" "}
+        <Link href="/about" sameWindow>
+          Next.js
+        </Link>{" "}
+        using <Link href="https://nextjs.org/docs/app">App router</Link>.{" "}
+        <Link href="https://maplibre.org/maplibre-gl-js/docs">MapLibre GL</Link>{" "}
+        map. GeoJSON served with{" "}
+        <Link href="https://nextjs.org/docs/pages/building-your-application/routing/api-routes">
+          Next API route
+        </Link>
+        .
+      </li>
+      <li>
+        <strong>data collection</strong> — Python script with{" "}
+        <Link href="https://github.com/codelucas/newspaper">
+          <code>newspaper3k</code>
+        </Link>{" "}
+        library.
+      </li>
+      <li>
+        <strong>database</strong> —{" "}
+        <Link href="https://supabase.com/">Supabase</Link> SQL db with three
+        tables to store articles, locations, and their relations.
+      </li>
+    </ul>
+    <h3 id="details">details</h3>
+    <p>
+      The Python script runs on a schedule in a{" "}
+      <Link href="https://github.com/features/actions">Github Action</Link>. It
+      checks against a{" "}
+      <Link href="https://github.com/ndemarchis/mapping-news/blob/main/public/feeds/feeds.csv">
+        predetermined set of RSS feeds
       </Link>
-      .
-    </p>
-    <h3>process</h3>
-    <p>
-      there are three main parts of this project: a recurring Python script, a
-      database, and the frontend.
+      , and determines whether any new articles have been added since it was
+      last checked. If so it will pull relevant information from those articles
+      and temporarily store them. The script runs more frequently during the
+      middle of the day and less frequently overnight.
     </p>
     <p>
-      the Python script is supposed to run on a schedule. it checks against a
-      predetermined set of RSS feeds, and determines whether any new articles
-      have been added since it last checked. if so, it will pull relevant
-      information from those articles, and temporarily store them.
-    </p>
-    <p>
-      it then uses OpenAI&apos;s{" "}
+      It then uses OpenAI’s{" "}
       <Link href="https://openrouter.ai/openai/gpt-4o-mini-2024-07-18">
-        <span className="font-mono">gpt-4o-mini-2024-07-18</span>
+        gpt-4o-mini-2024-07-18
       </Link>{" "}
-      model to extract physical locations that are mentioned in the text of each
-      article.
+      model with the following prompt to extract the physical locations
+      mentioned in the text of each article.
+    </p>
+    <blockquote className="px-4">
+      <p>
+        <code>
+          Your goal is to extract all points of interest and street addresses
+          from the text provided.
+        </code>
+      </p>
+    </blockquote>
+    <p>
+      After we have relevant locations extracted, we use the{" "}
+      <Link href="https://developers.google.com/maps/documentation/geocoding/overview">
+        Google Maps Geocoding API
+      </Link>{" "}
+      to determine their location. Any locations that are too generic to be
+      mapped at this proof-of-concept stage — for instance, general county or
+      state mentions — are removed.{" "}
     </p>
     <p>
-      after we have relevant locations extracted, we use the Google Maps
-      Geocoding API to determine their location, as well as to provide metadata
-      on whether the place is too generic to be mapped at this proof-of-concept
-      stage (for instance, counties, states). we can then use the{" "}
-      <span className="font-mono">place_id</span> that they provide to associate
-      articles with each other.
+      Our database then receives information about the locations, the articles,
+      and the relationship between the locations and articles.{" "}
     </p>
     <p>
-      we then send information on our locations, our articles and each relation
-      between an article and location to our database.
-    </p>
-    <p>
-      on the frontend, whenever someone loads the page, we use a{" "}
+      On the frontend, whenever someone loads the page we use a{" "}
       <Link href="https://nextjs.org/docs/app/building-your-application/routing/route-handlers">
         Next.js Route Handler
       </Link>{" "}
-      to reformat our database information as GeoJSON as requested. we then
-      serve that GeoJSON to an OpenLayers map.
+      to reformat our database information as GeoJSON. We then serve that
+      GeoJSON to an MapLibre GL map.{" "}
     </p>
     <p>
-      when a user clicks a location, we then use another route to handle
-      database requests, and pull the information of the articles that match the
-      selected location.
+      When a user clicks a location, we use another route to handle database
+      requests and pull the information of the articles that match the selected
+      location.{" "}
     </p>
-    <h3>blind spots</h3>
-    <p>
-      there are quite a few blind spots and limitations associated here. to name
-      a few:
-    </p>
+    <h3 id="blind-spots">blind spots</h3>
+    <p>There are issues, here are some:</p>
     <ul className="list-disc pl-4">
       <li>
-        the OpenAI model isn&apos;t amazing at pulling out non-obvious
-        locations, and sometimes is either too verbose or too general
+        The OpenAI model isn’t amazing at pulling out non-obvious locations, and
+        sometimes is either too verbose or too general.{" "}
       </li>
       <li>
-        at this moment, i can only really use publications that have working RSS
-        feeds. this is mostly fine, as most still do, but not all.
+        At this moment, we can only use publications that have working RSS
+        feeds. Most still do, but not all.{" "}
       </li>
       <li>
-        i did my best to filter feeds by their metro/local reporting, if
-        relevant. that obviously could leave a lot of state- or federal-level
-        news with local impact off the map.
+        We did our best to filter feeds by their metro/local reporting, when
+        relevant. That could leave state- or federal-level news stories that
+        have local impact off the map.{" "}
       </li>
     </ul>
-    <h3>next steps</h3>
+    <h3 id="next-steps">next steps</h3>
     <p>
-      like i said,{" "}
+      There’s a lot of work left to do in regards to addressing problems found
+      in the{" "}
       <Link href="https://github.com/ndemarchis/mapping-news/issues">
         issue tracker
       </Link>
-      . there&apos;s a <span className="italic">lot</span> of work to do —{" "}
-      <span className="italic">especially</span> to expand this work to more
-      communities and understand better how neighborhoods receive news coverage.
+      . One of our biggest goals is to expand this work to more communities and
+      better understand how neighborhoods receive news coverage.{" "}
     </p>
+    <h2 id="further-reading">further reading</h2>
+    <ul className="list-disc pl-4 [&>li]:pb-2">
+      <li>
+        Z. Metzger, “The State of Local News,” Local News Initiative. Accessed:
+        Dec. 31, 2024. [Online]. Available:{" "}
+        <Link href="https://localnewsinitiative.northwestern.edu/projects/state-of-local-news/2024/report/">
+          localnewsinitiative.northwestern.edu
+        </Link>
+      </li>
+      <li>
+        G. Ariyarathne and A. C. Nwala, “3DLNews: A Three-decade Dataset of US
+        Local News Articles,” in
+        <span className="italic">
+          Proceedings of the 33rd ACM International Conference on Information
+          and Knowledge Management (CIKM ’24)
+        </span>
+        , New York, NY, USA: ACM, 2024, pp. 1–5. doi:{" "}
+        <Link href="https://doi.org/10.1145/3627673.3679165">
+          10.1145/3627673.3679165
+        </Link>
+        .
+      </li>
+      <li>
+        <Link href="https://www.mediacloud.org/">Media Cloud</Link>
+      </li>
+    </ul>
   </div>
 );
 
