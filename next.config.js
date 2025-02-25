86552245d1d01e0b58c4bc02f7cb0c2947408d66
Nick DeMarchis <ned004@bucknell.edu>
2025-02-25 18:51:16 -0500

    two dev improvements

--- next.config.js
+++ next.config.js
@@ -14,6 +14,21 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 };
 
 module.exports = nextConfig;
